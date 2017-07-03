424416a87ad44b98e384702aa3daad15d35c198d
amar panray <amarpanray@gmail.com>
2017-07-03 17:15:09 -0400

    Basic Layout for 2 main pages with static values

--- PongKey/Models/ViewModels/DetailViewModel.cs
+++ PongKey/Models/ViewModels/DetailViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+//using System.Device.Location;
 
 namespace PongKey.Models.Welcome.ViewModels
 {
     public class DetailViewModel
     {
+        [Required]//possibly the only required field
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; } 
+        
+        public string Post { get; set; } 
+        public string Reply { get; set; }  
+        public string Guest { get; set; }            
     }
 }
